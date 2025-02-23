4c910b6eff02959b9552fbbc09da75a6057d4d88
Aleksa-S-L <aleksaluk715@gmail.com>
2025-02-23 15:36:34 +0200

    ДЗ: регулярний вираз для пошуку слів без 'A',внесені зміни

--- homework7.2.js
+++ homework7.2.js
@@ -4,6 +4,6 @@
 //  {6,} – минимум 6 символов
 //  \b – граница слова
 const text = "Wonderful Joyful Happiness Time Task Apple";
-const regex = /\b[^aA\s]{6,}\b/g;
-const matches = text.match(regex);
+const pattern = /\b[^aA\s]{6,}\b/g;
+const matches = text.match(pattern);
 console.log(matches); // ['Wonderful', 'Joyful']
